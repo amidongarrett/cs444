3ded40d4efd399ab9ff97d929cdde65d3aac3b45
Garrett Amidon <amidong@os-class.engr.oregonstate.edu>
2016-04-11 17:22:23 -0700

    Concurrent Progress, need random

--- Concurrent/concurrent1.c
+++ Concurrent/concurrent1.c
@@ -1,48 +1,83 @@
-#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-#define THREADNUM     5
-	pthread_mutex_t control;
+#include <pthread.h>
 
-void *produce(struct passData *data){
-	int wait, value, wait2;
-	struct passData temp;
-	wait = (rand() % 5) + 3;  //sleep for 3-7 seconds before producing
-	wait2 = (rand() % 8) + 2; //wait time to be passed
-	value = rand() % 100;  //value to be passed
-	sleep(wait);
-	// assign values to passData struct
-	temp.value = value;
-	temp.wait = wait2;
-	
-   
-}
 
-void *consume(struct passData *data){
-
-   
-}
+#define MAX 32       
+pthread_mutex_t control;
+pthread_cond_t conditionalCons, conditionalProd;
 
 struct passData{
 	int value;
 	int wait;
-};
+};	
 
-int main (int argc, char *argv[])
-{
-   pthread_t threads[THREADNUM];
-   int rc;
-   long t;
-   for(t=0; t<THREADNUM; t++){
-      printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-      if (rc){
-         printf("ERROR; return code from pthread_create() is %d\n", rc);
-         exit(-1);
-      }
-   }
+struct passData buffer[MAX];
+int bufferLength = 0;
 
-   /* Last thing that main() should do */
-   pthread_exit(NULL);
+
+
+void* producer(void *ptr) {
+    int i;
+	int wait;
+	  
+    while(1) {
+		wait = (rand() % 5) + 3; //sleep for 3-7 seconds before producing
+        pthread_mutex_lock(&control); /* protect buffer */
+		while(bufferLength == 31)
+			pthread_cond_wait(&conditionalProd, &control);
+			
+		buffer[bufferLength].wait = (rand() % 8) + 2; //wait time to be passed
+		buffer[bufferLength].value = rand() % 100;  //value to be passed
+		printf("Producer: Produced item with value %d \n", buffer[bufferLength].value);
+		bufferLength++;
+		pthread_cond_signal(&conditionalCons); /* wake up consumer */
+		pthread_mutex_unlock(&control); /* release the buffer */
+		sleep(wait);
+
+	}
+    pthread_exit(0);
 }
 
+void* consumer(void *ptr) {
+    int i;
+	while(1){
+         pthread_mutex_lock(&control); /* protect buffer */
+		while (bufferLength <= 0) /* If there is nothing in the buffer then wait */
+			pthread_cond_wait(&conditionalCons, &control);
+		pthread_mutex_unlock(&control); /* release the buffer */
+
+		sleep(buffer[bufferLength-1].wait);
+
+		pthread_mutex_lock(&control); /* protect buffer */
+		printf("Consumer: Consumed item with value %d\n", buffer[bufferLength-1].value);
+		bufferLength--;
+		pthread_mutex_unlock(&control); /* release the buffer */
+	}
+    pthread_exit(0);
+}
+
+int main(int argc, char **argv) {
+    pthread_t produce, consume;
+
+    // Initialize the mutex and condition variables
+    /* What's the NULL for ??? */
+    pthread_mutex_init(&control, NULL);
+    pthread_cond_init(&conditionalCons, NULL); /* Initialize consumer condition variable */
+    pthread_cond_init(&conditionalProd, NULL); /* Initialize producer condition variable */
+
+    // Create the threads
+    pthread_create(&consume, NULL, consumer, NULL);
+    pthread_create(&produce, NULL, producer, NULL);
+
+    // Wait for the threads to finish
+    // Otherwise main might run to the end
+    // and kill the entire process when it exits.
+    pthread_join(consume, NULL);
+    pthread_join(produce, NULL);
+
+    // Cleanup -- would happen automatically at end of program
+    pthread_mutex_destroy(&control); /* Free up control */
+    pthread_cond_destroy(&conditionalCons); /* Free up consumer condition variable */
+    pthread_cond_destroy(&conditionalProd); /* Free up producer condition variable */
+
+}
---
+++ Concurrent/concurrentTest.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <pthread.h>
+#include 
+
+#define MAX 32       
+pthread_mutex_t control;
+pthread_cond_t conditionalCons, conditionalProd;
+
+struct passData{
+	int value;
+	int wait;
+};	
+
+struct passData buffer[MAX];
+int bufferLength = 0;
+
+
+
+void* producer(void *ptr) {
+    int i;
+	int wait;
+	  
+    while(1) {
+		wait = (rand() % 5) + 3; //sleep for 3-7 seconds before producing
+        pthread_mutex_lock(&control); /* protect buffer */
+		while(bufferLength == 31)
+			pthread_cond_wait(&conditionalProd, &control);
+			
+		buffer[bufferLength].wait = (rand() % 8) + 2; //wait time to be passed
+		buffer[bufferLength].value = rand() % 100;  //value to be passed
+		printf("Producer: Produced item with value %d \n", buffer[bufferLength].value);
+		bufferLength++;
+		pthread_cond_signal(&conditionalCons); /* wake up consumer */
+		pthread_mutex_unlock(&control); /* release the buffer */
+		sleep(wait);
+
+	}
+    pthread_exit(0);
+}
+
+void* consumer(void *ptr) {
+    int i;
+	while(1){
+         pthread_mutex_lock(&control); /* protect buffer */
+		while (bufferLength <= 0) /* If there is nothing in the buffer then wait */
+			pthread_cond_wait(&conditionalCons, &control);
+		pthread_mutex_unlock(&control); /* release the buffer */
+
+		sleep(buffer[bufferLength-1].wait);
+
+		pthread_mutex_lock(&control); /* protect buffer */
+		printf("Consumer: Consumed item with value %d\n", buffer[bufferLength-1].value);
+		bufferLength--;
+		pthread_mutex_unlock(&control); /* release the buffer */
+	}
+    pthread_exit(0);
+}
+
+int main(int argc, char **argv) {
+    pthread_t produce, consume;
+
+    // Initialize the mutex and condition variables
+    /* What's the NULL for ??? */
+    pthread_mutex_init(&control, NULL);
+    pthread_cond_init(&conditionalCons, NULL); /* Initialize consumer condition variable */
+    pthread_cond_init(&conditionalProd, NULL); /* Initialize producer condition variable */
+
+    // Create the threads
+    pthread_create(&consume, NULL, consumer, NULL);
+    pthread_create(&produce, NULL, producer, NULL);
+
+    // Wait for the threads to finish
+    // Otherwise main might run to the end
+    // and kill the entire process when it exits.
+    pthread_join(consume, NULL);
+    pthread_join(produce, NULL);
+
+    // Cleanup -- would happen automatically at end of program
+    pthread_mutex_destroy(&control); /* Free up control */
+    pthread_cond_destroy(&conditionalCons); /* Free up consumer condition variable */
+    pthread_cond_destroy(&conditionalProd); /* Free up producer condition variable */
+
+}
